05cf06834d44fb13f07326a20e05fe47a5432355
NicolasBazan <nikobazan@hotmail.com>
2018-05-18 08:51:12 -0300

    mas ejercicios

--- clase_11.sql
+++ clase_11.sql
@@ -1,9 +1,11 @@
+#!
 SELECT country , count(*)
   FROM city
       INNER JOIN `country`
            USING (country_id)
   GROUP BY country , country_id;
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
+ #2
  SELECT country, count(*)
   FROM city
       INNER JOIN `country`
@@ -12,6 +14,7 @@ SELECT country , count(*)
  	HAVING count(*)>10
  	ORDER BY country desc;
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
+#3
 SELECT p.customer_id, c.first_name, c.last_name, a.address, count(1), sum(p.amount)
 	FROM customer c
 		INNER JOIN address a 
@@ -21,6 +24,7 @@ SELECT p.customer_id, c.first_name, c.last_name, a.address, count(1), sum(p.amou
 	GROUP BY c.customer_id
 	ORDER BY sum(p.amount) DESC;
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
+#4
 SELECT title 
 	FROM film 
 	WHERE film_id NOT 
@@ -36,10 +40,35 @@ SELECT title
 		INNER JOIN film USING(film_id));
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
+#5
 SELECT film.title, inventory_id
 FROM film 
 	INNER JOIN inventory USING (film_id)
 	LEFT JOIN rental USING (inventory_id)
 WHERE rental_id IS NULL;
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
-	
+#6
+SELECT c.first_name, c.last_name, c.store_id, f.title, r.rental_date
+FROM customer c 
+	INNER JOIN rental r 
+		ON c.customer_id = r.customer_id
+	INNER JOIN inventory i
+		ON 	r.inventory_id = i.inventory_id
+	INNER JOIN film f
+		ON i.film_id = f.film_id
+#WHERE r.return_date IS NOT NULL
+ORDER BY c.store_id, c.last_name;
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+#7
+SELECT st.store_id, ci.city, co.country, CONCAT(s.first_name,' ',s.last_name) AS Manager, SUM(p.amount)
+FROM store st
+	INNER JOIN address a USING(address_id)
+	INNER JOIN city ci USING(city_id)
+	INNER JOIN country co USING(country_id)
+	INNER JOIN staff s 
+		ON st.manager_staff_id = s.staff_id
+	INNER JOIN payment p USING(staff_id)
+GROUP BY st.store_id;
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+#8
+
